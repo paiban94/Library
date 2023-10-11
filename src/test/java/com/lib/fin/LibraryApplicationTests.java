be099bafc20fcf986c2c1741660c83788c7941d1
pabian94 <pabian94@naver.com>
2023-10-11 10:10:32 +0900

    1011

---
+++ src/test/java/com/lib/fin/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lib.fin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
