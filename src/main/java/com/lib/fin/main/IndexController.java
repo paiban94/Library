d6142ad8c1b5bb2f9e3570d6608ec3788961eaaf
Hwangyunje-github <dnjy1945@naver.com>
2023-10-17 12:29:02 +0900

    1017

---
+++ src/main/java/com/lib/fin/main/IndexController.java
@@ -0,0 +1,22 @@
+package com.lib.fin.main;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+	
+	@GetMapping("/")
+	@ResponseBody
+	public String getIndex() {
+		return "index";
+	}
+	
+	@GetMapping("/ksg")
+	public void ksg() {
+		
+	}
+
+}
