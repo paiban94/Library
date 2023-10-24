9116ddbf7ada96b628ee81f22b4e06b876a705e5
ksg0069 <ksg0069@naver.com>
2023-10-24 19:15:14 +0900

    .

--- src/main/java/com/lib/fin/approval/ApprovalService.java
+++ src/main/java/com/lib/fin/approval/ApprovalService.java
@@ -1,14 +1,17 @@
 package com.lib.fin.approval;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ApprovalService {
 	
+	@Value("${fin.upload}")
 	private String path;
 	
+	@Value("${fin.approval.draft}")
 	private String approvalName;
 	
 	@Autowired
