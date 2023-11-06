3a4b536da28b8dcc573a00ad42d86037edd938c4
Hwangyunje-github <dnjy1945@naver.com>
2023-11-07 08:40:06 +0900

    1107

--- src/main/java/com/lib/fin/attendance/AttendanceVO.java
+++ src/main/java/com/lib/fin/attendance/AttendanceVO.java
@@ -1,5 +1,18 @@
 package com.lib.fin.attendance;
 
-public class AttendanceVO {
+import java.sql.Date;
 
+import com.lib.fin.commons.CommonVO;
+
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+public class AttendanceVO extends CommonVO{
+
+	private int atd_no;
+	private String emp_no;
+	private Date gtw_time;
+	private Date lw_time;
+	private String status;
 }
